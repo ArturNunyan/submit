e49920e92482302cd4960b7f0aa5f5a21ec07d5a
ArturNunyan <118257851+ArturNunyan@users.noreply.github.com>
2023-04-02 03:03:13 +0400

    error validation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  
+let fullInfo = {}
+
+const [firstName, setFirstName] = useState("");
+const [lastName, setLastName] = useState(""); 
+
+const [email, setEmail] = useState("");
+const [error, setError] = useState("");
+
+function isValidEmail(email){
+  return  /\S+@\S+\.\S+/.test(email);
+}
+
+const hendleSubmit = (event) => {
+  event.preventDefault();
+  fullInfo.firstName = firstName;
+  fullInfo.lastName = lastName;
+  fullInfo.email = email;
+  console.log(fullInfo);
+}
 
   return (
     <div className="App">
       <h1>Submit Your <br/> personal information</h1>
-      <form>
-        <input type="text" placeholder="First name"/>
-        <input type="text" placeholder="Last name"/>
+      <form onSubmit={hendleSubmit}>
+        <input type="text" placeholder="First name" onChange={(e) => setFirstName(e.target.value)}/>
+        <input type="text" placeholder="Last name" onChange={(e) => setLastName(e.target.value)}/>
         <p>
-        <input type="text" placeholder="E-mail"/>
+        <input 
+          type="text" 
+          placeholder="E-mail" 
+          onChange={(e) =>{
+            if (!isValidEmail(e.target.value)) {
+              setError('Email is invalid');
+            } else {
+              setError(null);
+            }
+            setEmail(e.target.value);
+          }}
+        />
+        <br/>
+        <span className='errorMessege'>{error}</span>
         </p>
         <input className="blueButton" type="submit" value="Next >"/>
       </form>
